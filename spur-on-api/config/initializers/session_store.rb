fa788b85a63a3ace50a437e3ac89cb812477b5b5
Dominic Fannjiang <dfannjiang@gmail.com>
2023-01-22 13:47:16 -0800

    run rubocop

--- spur-on-api/config/initializers/session_store.rb
+++ spur-on-api/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_spur-on-api', domain: 'name-of-you-app-json-api'
+  Rails.application.config.session_store(:cookie_store, key: '_spur-on-api', domain: 'name-of-you-app-json-api')
 else
-    Rails.application.config.session_store :cookie_store, key: '_spur-on-api'
+  Rails.application.config.session_store(:cookie_store, key: '_spur-on-api')
 end
