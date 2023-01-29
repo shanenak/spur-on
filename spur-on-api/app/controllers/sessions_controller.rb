be40be822cb62f16f5a57bd603c85360161686bb
Dominic Fannjiang <dfannjiang@gmail.com>
2023-01-29 13:23:18 -0800

    handle wrong password

--- spur-on-api/app/controllers/sessions_controller.rb
+++ spur-on-api/app/controllers/sessions_controller.rb
@@ -2,12 +2,19 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: session_params[:username])
 
-        if @user && @user.authenticate(session_params[:password])
-            login!
-            render json: {
-            logged_in: true,
-            user: @user
-            }
+        if @user
+            if @user.authenticate(session_params[:password])
+                login!
+                render json: {
+                logged_in: true,
+                user: @user
+                }
+            else
+                render json: {
+                status: 401,
+                error: 'Incorrect password, please try again'
+                }
+            end
         else
             render json: {
             status: 401,
