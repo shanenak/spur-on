b7c3140f12a484567e44f7a057c1648d48b279f3
Dominic Fannjiang <dfannjiang@users.noreply.github.com>
2023-01-29 19:58:14 -0800

    Add bottom tab navigation and user screen (#6)

--- spurOnApp/src/screens/HomeScreen/HomeScreen.tsx
+++ spurOnApp/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,36 +1,14 @@
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 import {View, Text} from 'react-native';
-import axios from 'axios';
 
 import {useCurrentUser} from '../../context/UserContext';
-import CustomButton from '../../components/CustomButton';
-import {defaultUsername} from '../../context/UserContext';
 
 const HomeScreen: FC = () => {
   const {username, setUsername} = useCurrentUser();
-  const [logoutError, setLogoutError] = useState('');
-
-  const onSignOutPressed = () => {
-    axios
-      .post('http://localhost:3001/logout', {withCredentials: true})
-      .then(response => {
-        setUsername(defaultUsername);
-      })
-      .catch(error => {
-        console.log('api errors:', error);
-        setLogoutError(error.response);
-      });
-  };
 
   return (
     <View>
       <Text>Home! User: {username}</Text>
-      <CustomButton
-        text={'Logout'}
-        onPress={onSignOutPressed}
-        type={'TERTIARY'}
-      />
-      {logoutError != '' && <Text style={{color: 'red'}}>{logoutError}</Text>}
     </View>
   );
 };
